78f9c84b2f9b461c2f88fcee5be314390203cd77
Donovan van Heerden <dp.vanHeerden@gmail.com>
2016-02-11 21:12:52 +0200

    Major Change to js file and styling.
    Altered printArray method -> MapJsonToTree
    Removed the init jQuery to a function -> setTreeFunctionality
    
    Function MapJsonToTree also identifies specific file types and sets
    their icon appropriately.

--- js/tree-nav.js
+++ js/tree-nav.js
@@ -1,49 +1,132 @@
-var xml_html = "";
 
-$(function () {
-
-    LoadDir();
-
-    $('.tree li:has(ul)').addClass('parent_li').find(' > span').attr('title', 'Open this folder');
-    $('.tree li:not(ul) li:not(.parent_li)').each(function() {
-    		var folder = $(this).children().children(); // get the <i> and change the icon to open folder for empty folders
-        if (folder.hasClass('fa-folder-o')) {
-        	folder.removeClass('fa fa-folder-o fa-lg');
-          folder.addClass('fa fa-folder-open-o fa-lg');
-        }
-    });
-
-    $('.tree li.parent_li > span').on('click', function (e) {
-    		var children = $(this).parent('li.parent_li').find(' > ul > li');
-        if (children.is(":visible")) {
-            children.hide('fast');
-            $(this).children().first().removeClass('fa fa-folder-open-o fa-lg').addClass('fa fa-folder-o fa-lg');
-            $(this).attr('title', 'Open this folder');
-        } else {
-            children.show('fast');
-            $(this).children().first().removeClass('fa fa-folder-o fa-lg').addClass('fa fa-folder-open-o fa-lg');
-            $(this).attr('title', 'Close this folder');
-        }
-        e.stopPropagation();
-    });
-});
-
-function printArray(arr){
+// Recursive Method to Navigate through the JSON Object and append html list
+// items to create the directory tree.
+function MapJsonToTree(arr){
+  var html = "";
     for(var i = 0; i < arr.length; i++){
-        if(arr[i].children instanceof Array){
-          console.log(arr[i]);
-            printArray(arr[i].children);
+        if(arr[i].children instanceof Array && arr[i].isFolder){
+          if (arr[i].children.length > 0)
+          {
+              html += '<li><span><label><i class="fa fa-folder-o fa-lg"></i>' + arr[i].title + '</label></span><ul>';
+          } else {
+            html += '<li><span><label><i class="fa fa-folder-open-o fa-lg"></i>' + arr[i].title + '</label></span> <ul>';
+          }
+
+          html += MapJsonToTree(arr[i].children);
+          html += '</ul></li>';
 
         }else{
-            console.log(arr[i]);
+          var extention = arr[i].title.substring(arr[i].title.lastIndexOf('.'), arr[i].title.length);
+          switch (extention) {
+            case '.cs':
+              html += '<li><span><label><i class="fa fa-file-code-o fa-lg"></i>' + arr[i].title + '</label></span></li>';
+              break;
+            case '.css':
+              html += '<li><span><label><i class="fa fa-file-code-o fa-lg"></i>' + arr[i].title + '</label></span></li>';
+              break;
+            case '.scss':
+              html += '<li><span><label><i class="fa fa-file-code-o fa-lg"></i>' + arr[i].title + '</label></span></li>';
+              break;
+            case '.less':
+              html += '<li><span><label><i class="fa fa-file-code-o fa-lg"></i>' + arr[i].title + '</label></span></li>';
+              break;
+            case '.aspx':
+              html += '<li><span><label><i class="fa fa-file-code-o fa-lg"></i>' + arr[i].title + '</label></span></li>';
+              break;
+            case '.asmx':
+              html += '<li><span><label><i class="fa fa-file-code-o fa-lg"></i>' + arr[i].title + '</label></span></li>';
+              break;
+            case '.disco':
+              html += '<li><span><label><i class="fa fa-file-code-o fa-lg"></i>' + arr[i].title + '</label></span></li>';
+              break;
+            case '.discomap':
+              html += '<li><span><label><i class="fa fa-file-code-o fa-lg"></i>' + arr[i].title + '</label></span></li>';
+              break;
+            case '.wsdl':
+              html += '<li><span><label><i class="fa fa-file-code-o fa-lg"></i>' + arr[i].title + '</label></span></li>';
+              break;
+            case '.master':
+              html += '<li><span><label><i class="fa fa-file-code-o fa-lg"></i>' + arr[i].title + '</label></span></li>';
+              break;
+            case '.webinfo':
+              html += '<li><span><label><i class="fa fa-file-code-o fa-lg"></i>' + arr[i].title + '</label></span></li>';
+              break;
+            case '.config':
+              html += '<li><span><label><i class="fa fa-file-code-o fa-lg"></i>' + arr[i].title + '</label></span></li>';
+              break;
+            case '.publishproj':
+              html += '<li><span><label><i class="fa fa-file-code-o fa-lg"></i>' + arr[i].title + '</label></span></li>';
+              break;
+            case '.js':
+              html += '<li><span><label><i class="fa fa-file-code-o fa-lg"></i>' + arr[i].title + '</label></span></li>';
+              break;
+            case '.json':
+              html += '<li><span><label><i class="fa fa-file-code-o fa-lg"></i>' + arr[i].title + '</label></span></li>';
+              break;
+            case '.png':
+              html += '<li><span><label><i class="fa fa-file-image-o fa-lg"></i>' + arr[i].title + '</label></span></li>';
+              break;
+            case '.jpg':
+              html += '<li><span><label><i class="fa fa-file-image-o fa-lg"></i>' + arr[i].title + '</label></span></li>';
+              break;
+            case '.jpeg':
+              html += '<li><span><label><i class="fa fa-file-image-o fa-lg"></i>' + arr[i].title + '</label></span></li>';
+              break;
+            case '.psd':
+              html += '<li><span><label><i class="fa fa-file-image-o fa-lg"></i>' + arr[i].title + '</label></span></li>';
+              break;
+            case '.ico':
+              html += '<li><span><label><i class="fa fa-file-image-o fa-lg"></i>' + arr[i].title + '</label></span></li>';
+              break;
+            case '.zip':
+              html += '<li><span><label><i class="fa fa-file-zip-o fa-lg"></i>' + arr[i].title + '</label></span></li>';
+              break;
+            case '.rar':
+              html += '<li><span><label><i class="fa fa-file-zip-o fa-lg"></i>' + arr[i].title + '</label></span></li>';
+              break;
+            case '.7zip':
+              html += '<li><span><label><i class="fa fa-file-zip-o fa-lg"></i>' + arr[i].title + '</label></span></li>';
+              break;
+            case '.accdb':
+              html += '<li><span><label><i class="fa fa-database fa-lg"></i>' + arr[i].title + '</label></span></li>';
+              break;
+            default:
+              html += '<li><span><label><i class="fa fa-file-text-o fa-lg"></i>' + arr[i].title + '</label></span></li>';
+          }
         }
     }
+    return html;
+}
+
+function setTreeFunctionality() {
+  $('.tree li:has(ul)').addClass('parent_li').find(' > span');
+  $('.tree li:not(ul) li:not(.parent_li)').each(function() {
+      var folder = $(this).children().children(); // get the <i> and change the icon to open folder for empty folders
+      if (folder.hasClass('fa-folder-o')) {
+        folder.removeClass('fa fa-folder-o fa-lg');
+        folder.addClass('fa fa-folder-open-o fa-lg');
+      }
+  });
+
+  $('.tree li.parent_li > span').on('click', function (e) {
+      var children = $(this).parent('li.parent_li').find(' > ul > li');
+      if (children.is(":visible")) {
+          children.hide('fast');
+          $(this).children().children().first().removeClass('fa fa-folder-open-o fa-lg').addClass('fa fa-folder-o fa-lg');
+          $(this).attr('title', 'Open this folder');
+      } else {
+          children.show('fast');
+          $(this).children().children().first().removeClass('fa fa-folder-o fa-lg').addClass('fa fa-folder-open-o fa-lg');
+          $(this).attr('title', 'Close this folder');
+      }
+      e.stopPropagation();
+  });
 }
 
 $(document).ready(function(){
   $.ajax({
     type: "GET",
-    url: "./online_help.json",
+    url: "./vs_2013_mapped.json",
     dataType: "text",
     success: function(json) {
         var t = $.parseJSON(json);
@@ -51,7 +134,9 @@ $(document).ready(function(){
         console.log(arr);
         var obj = arr[0];
 
-        printArray(obj.children);
+        $('.tree ul').html(MapJsonToTree(obj.children));
+
+        setTreeFunctionality();
       },
 
     error: function(jqXHR, exception) {
@@ -72,6 +157,11 @@ $(document).ready(function(){
           }
       }
   });
+
+
+
+
+
 });
 
 
